1040b5c8a6cee56152cc269fa4f1e691ba0c8345
x1ah <x1ahgxq@gmail.com>
2017-06-07 09:31:51 +0800

    完成聊天气泡，发送接收气泡显示，以及大部分前端逻辑 TODO：判断消息发送者，气泡三角方向不一

--- chatrm/main.py
+++ chatrm/main.py
@@ -5,9 +5,9 @@ import uuid
 import time
 from functools import wraps
 
-from flask import Flask, render_template, redirect, request
+from flask import Flask, render_template, redirect, request, flash
 from flask_socketio import SocketIO, emit
-from flask_login import LoginManager, current_user, login_user
+from flask_login import LoginManager, current_user, login_user, logout_user
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = str(uuid.uuid4())
@@ -18,9 +18,10 @@ user_manager = None
 
 class User(object):
 
-    def __init__(self, username, password):
-        self.uid = int(time.time() * 100)
+    def __init__(self, username, password, nickname=''):
+        self.uid = user_manager.generate_uid
         self.username = username
+        self.nickname = nickname or username
         # TODO: 密码存储
         self.password = password
         self.join_time = time.time()
@@ -39,7 +40,7 @@ class User(object):
         return False
 
     def get_id(self):
-        return self.uid
+        return self.username
 
 
 class UserManager(object):
@@ -50,17 +51,24 @@ class UserManager(object):
     """
 
     def __init__(self, UserDB=dict()):
-        # nametuple('user', 'uid username password join_time last_active_time')
+        # nametuple('user', 'uid nickname password join_time last_active_time')
         self.db = UserDB
+        self.db['user_nums'] = 0
 
-    def create(self, user: User):
-        if user.uid not in self.db:
-            self.db[user.uid] = user
+    def create(self, username, password):
+        if username not in self.db:
+            self.db[username] = User(username, password)
+            self.db['user_nums'] += 1
+            return self.db.get(username)
         else:
-            return self.db.get(user.uid)
+            raise Exception("user exist")
 
-    def load(self, user_id):
-        return self.db.get(user_id, None)
+    def load(self, username):
+        return self.db.get(username, None)
+
+    @property
+    def generate_uid(self):
+        return self.db.get('user_nums') + 1
 
 @login_manager.user_loader
 def load_user(user_id):
@@ -68,7 +76,7 @@ def load_user(user_id):
 
 def login_required(redirect_to='/login'):
     def decorate(f):
-        @wraps(decorate)
+        @wraps(f)
         def fc(*args, **kw):
             if current_user.is_authenticated:
                 return f(*args, **kw)
@@ -77,10 +85,34 @@ def login_required(redirect_to='/login'):
         return fc
     return decorate
 
+
 @app.route('/', methods=['GET', 'POST'])
 @login_required()
 def index():
     return render_template("index.html")
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'GET':
+        return render_template('register.html')
+    elif request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+
+        # 前端验证填入字段
+
+        # try:
+        #     user = user_manager.create(username, password)
+        # except:
+        #     flash("用户名已被注册")
+        #     return redirect('/register')
+        user = user_manager.create(username, password)
+        login_user(user)
+        return redirect('/')
+    else:
+        raise NotImplementedError
+
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -89,12 +121,24 @@ def login():
     elif request.method == 'POST':
         username = request.form.get('username')
         password = request.form.get('password')
-        user = User(username, password)
-        user_manager.create(user)
+        user = user_manager.load(username)
+        if not user:
+            flash("用户不存在")
+            return redirect('/login')
+        if password != user.password:
+            flash("密码不正确")
+            return redirect('/login')
         login_user(user)
-        return "login as {username} {password}".format(username=username, password=password)
+        return redirect('/')
     else:
         raise NotImplementedError
+
+
+@app.route('/logout')
+@login_required()
+def logout():
+    logout_user()
+    return redirect('/login')
 
 @socketio.on('message')
 def chat(msg):
--- chatrm/static/js/chatroom.js
+++ chatrm/static/js/chatroom.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+
   $(".login").click(function(){
     $.post("/login",
       {
@@ -10,4 +11,53 @@ $(document).ready(function(){
         window.location.href = "/";
       });
     });
+
+  $(".registerPage").click(function(){
+    window.location.href = "/register";
+  });
+
+  $(".register").click(function(){
+    $.post("/register",
+      {
+        username: $("#username").val(),
+        password: $("#password").val()
+      },
+      function(result, status) {
+        window.location.href = "/";
+      });
+  });
+
+  $(function () {
+    var socket = io();
+    socket.on('connect', function() {
+      socket.emit('message', {msg: 'user connected'});
+      console.log('user connected');
+    });
+
+    socket.on('disconnect', function() {
+      socket.emit('message', {msg: 'user disconnected'});
+    });
+
+    socket.on('response', function(msg){
+      $('#messages').append($('<li>').text(msg));
+      var empty_div = $('<div />');
+      var send_div = $('<div />', {"class": "sender"});
+      var avater = $('<img />', {"src": "chatTemplateExample2_files/cat.jpg"});
+      avater.appendTo(empty_div);
+      empty_div.appendTo(send_div);
+      var empty_div = $('<div />');
+      var chat_box = $('<div />', {"class": "left_triangle"});
+      var chat_content = $('<span />').text(msg);
+      chat_box.appendTo(empty_div);
+      chat_content.appendTo(empty_div);
+      empty_div.appendTo(send_div);
+      var msghsty = $('.msgHistory');
+      send_div.appendTo(msghsty);
+    });
+    $('form').submit(function(){
+      socket.emit('message', {msg: $('#inputMsg').val()});
+      $('#inputMsg').val('');
+      return false;
+    });
+  });
 });
